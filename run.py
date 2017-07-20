036d0562f38675978b7ac9e34971206c5231dc92
Alexander Rusetski <alex.rus@core-tech.ru>
2017-07-20 11:49:19 +0300

    xz

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ def write_history_file(filename, lst):
 				timestamp = int(str[1:])
 				dt = datetime.fromtimestamp(timestamp)
 				date_string = '%02d.%02d.%02d %02d:%02d\n' %(dt.day, dt.month, dt.year, dt.hour, dt.minute)
+				print('%s %s' %(timestamp, date_string))
 				file.write('#%s' %(date_string))
 			else:
 				file.write(str + '\n')
